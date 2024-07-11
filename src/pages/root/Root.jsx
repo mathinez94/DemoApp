df0fb97c6370bf36a3d32e707ab8dcf85a155d36
mathinez94 <audumathias@gmail.com>
2024-07-11 19:45:45 +0100

    nav bar mobile view fixed

--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../root/Root.css'
-import {  Link, NavLink, Outlet } from 'react-router-dom'
+import {  Link, NavLink, Outlet } from 'react-router-dom';
+import { FaTimesCircle, FaBook } from "react-icons/fa";
 
 export const Root = () => {
+
+  const [menu, setMenu] = useState(false)
+  console.log(menu)
   return (
     <>
     <div className='root' >
 
       <Link  className="logo"><div>University of Nebraska</div></Link>
 
-      <nav className="links">            
-          <NavLink to='/' className='navlink'>Home</NavLink> 
-          <NavLink to='/research' className='navlink'>Research</NavLink>
-          <NavLink to='/Peoples' className='navlink'>People</NavLink>
-          <NavLink to='/Publications' className='navlink'>Publications</NavLink> 
-          <NavLink to='/Teaching' className='navlink'>Teaching</NavLink> 
-          <NavLink to='/Opportunities' className='navlink'>Opportunities</NavLink>        
-          <NavLink to='/News' className='navlink'>News</NavLink>
-      </nav>
+      <div className="mobileView">
+        <div className="menu" onClick={() =>setMenu(!menu)}>{menu ? <FaTimesCircle className='mobileCloseIcon'/> : <FaBook className='mobileHomeIcon'/>}</div>
+        <nav className={`links ${menu ? 'ViewLink' : ""}`}>            
+            <NavLink to='/' className='navlink'>Home</NavLink> 
+            <NavLink to='/research' className='navlink'>Research</NavLink>
+            <NavLink to='/Peoples' className='navlink'>People</NavLink>
+            <NavLink to='/Publications' className='navlink'>Publications</NavLink> 
+            <NavLink to='/Teaching' className='navlink'>Teaching</NavLink> 
+            <NavLink to='/Opportunities' className='navlink'>Opportunities</NavLink>        
+            <NavLink to='/News' className='navlink'>News</NavLink>
+        </nav>
+      </div>
     </div>
     
     <Outlet />
